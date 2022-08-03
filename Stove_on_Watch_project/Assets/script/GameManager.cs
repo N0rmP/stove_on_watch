10bea851f52057e38b350629b89fe96abb7213e3
N0rmP <cc4607950@gmail.com>
2022-08-03 17:05:35 +0900

    order_list 정리정돈, 기억 안 나지만 자잘한 정리정돈, update에 사용될 on 함수 3개 추가, 행동의 initial_max_cost 수정 방법 강구

--- Stove_on_Watch_project/Assets/script/GameManager.cs
+++ Stove_on_Watch_project/Assets/script/GameManager.cs
@@ -13,20 +13,19 @@ public class GameManager : MonoBehaviour
     private bool is_Plr_turn;
 
     //public delegate void order_func(int i);
-    Queue<abst_action> order_list;
+    public Queue<abst_action> order_list;
+
+    private player Plr;
 
     #region combat
     private bool combat_process() {   //★무한루프로 인해 게임 정지 시, 병렬 처리 검색 필요
-        order_func temp_order;
-        bool temp_Plr_turn = true;
+        bool temp_Plr_turn = true;  //describes whose turn is it now during current acction, it can be different from it in actual turn
         int temp_value = 0;
 
         while (true) {
             //★if is_Plr_turn : on_Plr_turn_start
             while (temp_Plr_turn == this.is_Plr_turn | order_list.Count > 0) {
-                if (order_list.Count > 0) {
-                    order_list.Dequeue()(order_value_list.Dequeue());
-                }
+                order_list.Dequeue().use();
                 Thread.Sleep(100); //★유니티가 통째로 멈출 가능성 높음
             }
             temp_Plr_turn = this.is_Plr_turn;
@@ -48,22 +47,11 @@ public class GameManager : MonoBehaviour
     #region get_set
     public bool get_is_Plr_turn() { return this.is_Plr_turn; }
     public void set_is_Plr_turn(bool b) { is_Plr_turn = b; } //player calls it with button instantly to prevent click turn_end twice, enemy calls it in its function directly anyway (it's no problem)
+    public player get_Plr() { return Plr; }
     #endregion get_set
 
     void Awake() {
         if (g == null) { g = this; } else { Destroy(this.gameObject); }
         DontDestroyOnLoad(this.gameObject);
-    }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
